4f01fa3252bea2cc7f63e14511e3c606bf460073
ferdi riansyah <ferdi.rs17@gmail.com>
2022-08-30 15:53:28 +0700

    Initial commit

---
+++ app/src/main/java/id/co/sistema/vkey/CustomAplication.kt
@@ -0,0 +1,288 @@
+package id.co.sistema.vkey
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.os.Parcelable
+import android.text.TextUtils
+import android.util.Log
+import com.vkey.android.internal.vguard.engine.BasicThreatInfo
+import com.vkey.android.vguard.*
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CustomAplication :Application(), VGExceptionHandler , Application.ActivityLifecycleCallbacks {
+
+    private var vGuardMgr: VGuard? = null
+
+    // LifecycleHook to notify VGuard of activity's lifecycle
+    private lateinit var hook: VGuardLifecycleHook
+
+    // For VGuard to notify host app of events
+    private lateinit var broadcastRcvr: VGuardBroadcastReceiver
+
+    companion object {
+        private const val PROFILE_LOADED = "vkey.android.vguard.PROFILE_LOADED"
+        private const val VOS_FIRMWARE_RETURN_CODE_KEY = "vkey.android.vguard.FIRMWARE_RETURN_CODE"
+        private const val PROFILE_THREAT_RESPONSE = "vkey.android.vguard.PROFILE_THREAT_RESPONSE"
+        private const val TAG_ON_RECEIVE = "OnReceive"
+        private const val TAG_VGUARD_STATUS = "VGuardStatus"
+        private const val TAG_VOS_READY = "VosReady"
+        private const val TAG_VGUARD_MESSAGE = "VguardMessage"
+        private const val TAG_HANDLE_THREAT_POLICY = "HandleThreat"
+        private const val TAG = "CustomApplication"
+        private const val DEFAULT_VALUE = 0L
+    }
+
+    private fun setupVGuard(activity: Activity) {
+
+        receiveVGuardBroadcast(activity)
+
+        // register using LocalBroadcastManager only for keeping data within your app
+        registerLocalBroadcast()
+
+        /**
+         * Using new configuration method getVGuard(context, config)
+         * */
+        setupAppProtection()
+    }
+
+    private fun receiveVGuardBroadcast(activity: Activity) {
+        broadcastRcvr = object : VGuardBroadcastReceiver(activity) {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                super.onReceive(context, intent)
+
+                when {
+                    PROFILE_LOADED == intent?.action -> Log.d(
+                        TAG_ON_RECEIVE,
+                        "PROFILE_LOADED"
+                    )
+
+                    ACTION_SCAN_COMPLETE == intent?.action -> Log.d(
+                        TAG_ON_RECEIVE,
+                        "ACTION_SCAN_COMPLETE"
+                    )
+
+                    VGUARD_OVERLAY_DETECTED_DISABLE == intent?.action -> Log.d(
+                        TAG_ON_RECEIVE,
+                        "VGUARD_OVERLAY_DETECTED_DISABLE"
+                    )
+
+                    VGUARD_OVERLAY_DETECTED == intent?.action -> Log.d(
+                        TAG_ON_RECEIVE,
+                        "VGUARD_OVERLAY_DETECTED"
+                    )
+
+                    VGUARD_STATUS == intent?.action -> {
+                        Log.d(
+                            TAG_VGUARD_STATUS,
+                            "HasExtraVGuardInitStatus: ${intent.hasExtra(VGUARD_INIT_STATUS)}"
+                        )
+
+                        if (intent.hasExtra(VGUARD_MESSAGE)) {
+                            val message = intent.getStringExtra(VGUARD_MESSAGE)
+                            var allMessage = "\n $VGUARD_MESSAGE : $message"
+                            if (message != null) {
+                                Log.d("MSG", message)
+                            }
+                            Log.d(TAG_VGUARD_MESSAGE, allMessage)
+                        }
+
+                        if (intent.hasExtra(VGUARD_HANDLE_THREAT_POLICY)) {
+                            val detectedThreats =
+                                intent.getParcelableArrayListExtra<Parcelable>(SCAN_COMPLETE_RESULT)
+                            val builder = StringBuilder()
+
+                            if (detectedThreats != null) {
+                                for (info in detectedThreats) {
+                                    val infoStr = (info as BasicThreatInfo).toString()
+                                    builder.append("$infoStr \n")
+                                }
+
+                                val highestResponse =
+                                    intent.getIntExtra(VGUARD_HIGHEST_THREAT_POLICY, -1)
+                                val alertTitle = intent.getStringExtra(VGUARD_ALERT_TITLE)
+                                val alertMessage = intent.getStringExtra(VGUARD_ALERT_MESSAGE)
+                                val disabledAppExpired =
+                                    intent.getLongExtra(VGUARD_DISABLED_APP_EXPIRED, 0)
+
+                                when {
+                                    highestResponse > 0 -> builder.append("highest policy: $highestResponse\n")
+                                    !TextUtils.isEmpty(alertTitle) -> builder.append("alertTitle: $alertTitle\n")
+                                    !TextUtils.isEmpty(alertMessage) -> builder.append("alertMessage: $alertMessage\n")
+                                    disabledAppExpired > 0 -> {
+                                        val format = SimpleDateFormat(
+                                            "yyyy-MMdd HH:mm:ss",
+                                            Locale.getDefault()
+                                        )
+                                        val activeDate = format.format(Date(disabledAppExpired))
+                                        builder.append("App can use again after: $activeDate\n")
+                                    }
+                                }
+
+                                Log.d(TAG_HANDLE_THREAT_POLICY, builder.toString())
+                            }
+                        }
+
+                        if (intent.hasExtra(VGUARD_INIT_STATUS)) {
+                            Log.d(
+                                TAG_VGUARD_STATUS,
+                                "VGUARD_INIT_STATUS: ${
+                                    intent.getBooleanExtra(
+                                        VGUARD_INIT_STATUS,
+                                        false
+                                    )
+                                }"
+                            )
+                            val initStatus = intent.getBooleanExtra(VGUARD_INIT_STATUS, false)
+                            var message = "\n $VGUARD_STATUS: $initStatus"
+
+                            if (!initStatus) {
+                                try {
+                                    val jsonObject =
+                                        JSONObject(intent.getStringExtra(VGUARD_MESSAGE))
+                                    Log.d(
+                                        TAG_VGUARD_STATUS,
+                                        "code: ${jsonObject.getString("code")}"
+                                    )
+                                    Log.d(
+                                        TAG_VGUARD_STATUS,
+                                        "code: ${jsonObject.getString("description")}"
+                                    )
+                                    message += jsonObject.toString()
+                                } catch (e: Exception) {
+                                    Log.e(TAG_VGUARD_STATUS, e.message.toString())
+                                    e.printStackTrace()
+                                }
+                                Log.d(TAG_VGUARD_STATUS, message)
+                            }
+                        }
+
+                        if (intent.hasExtra(VGUARD_SSL_ERROR_DETECTED)) {
+                            Log.d(
+                                TAG_VGUARD_STATUS,
+                                "VGUARD_SSL_ERROR_DETECTED: ${
+                                    intent.getBooleanExtra(
+                                        VGUARD_SSL_ERROR_DETECTED,
+                                        false
+                                    )
+                                }"
+                            )
+                            val sslError = intent.getBooleanExtra(VGUARD_SSL_ERROR_DETECTED, false)
+                            var message = "\n $VGUARD_SSL_ERROR_DETECTED: $sslError"
+
+                            if (sslError) {
+                                try {
+                                    val jsonObject =
+                                        JSONObject(intent.getStringExtra(VGUARD_MESSAGE))
+                                    Log.d(
+                                        TAG_VGUARD_STATUS,
+                                        jsonObject.getString(VGUARD_ALERT_TITLE)
+                                    )
+                                    Log.d(
+                                        TAG_VGUARD_STATUS,
+                                        jsonObject.getString(VGUARD_ALERT_MESSAGE)
+                                    )
+                                    message += jsonObject.toString()
+                                } catch (e: Exception) {
+                                    Log.e(TAG_VGUARD_STATUS, e.message.toString())
+                                    e.printStackTrace()
+                                }
+                            }
+                        }
+                    }
+
+                    VOS_READY == intent?.action -> {
+                        val firmwareReturnCode =
+                            intent.getLongExtra(VOS_FIRMWARE_RETURN_CODE_KEY, DEFAULT_VALUE)
+                        if (firmwareReturnCode >= DEFAULT_VALUE) {
+                            // if the `VGuardManager` is not available,
+                            // create a `VGuardManager` instance from `VGuardFactory`
+                            if (vGuardMgr == null) {
+                                vGuardMgr = VGuardFactory.getInstance()
+                                hook = ActivityLifecycleHook(vGuardMgr)
+
+                                val isStarted = vGuardMgr?.isVosStarted.toString()
+                                val valueTID = vGuardMgr?.troubleshootingId.toString()
+                                Log.d(TAG_VOS_READY, "isVosStarted: $isStarted")
+                                Log.d(TAG_VOS_READY, "TID: $valueTID")
+                            }
+                        } else {
+                            // Error handling
+                            Log.d(TAG_VOS_READY, "vos_ready_error_firmware")
+                        }
+                        Log.d(TAG_VOS_READY, "VOS_READY")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun registerLocalBroadcast() {
+        LocalBroadcastManager.getInstance(this).apply {
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.ACTION_FINISH))
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.ACTION_SCAN_COMPLETE))
+            registerReceiver(broadcastRcvr, IntentFilter(PROFILE_LOADED))
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.VOS_READY))
+            registerReceiver(broadcastRcvr, IntentFilter(PROFILE_THREAT_RESPONSE))
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.VGUARD_OVERLAY_DETECTED))
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.VGUARD_OVERLAY_DETECTED_DISABLE))
+            registerReceiver(broadcastRcvr, IntentFilter(VGuardBroadcastReceiver.VGUARD_STATUS))
+        }
+    }
+
+    private fun setupAppProtection() {
+        try {
+            val config = VGuardFactory.Builder()
+                .setDebugable(true)
+                .setAllowsArbitraryNetworking(true)
+                .setMemoryConfiguration(MemoryConfiguration.DEFAULT)
+                .setVGExceptionHandler(this)
+
+            VGuardFactory().getVGuard(this, config)
+        } catch (e: Exception) {
+            Log.e(TAG, e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(this)
+    }
+
+    override fun handleException(e: Exception?) {}
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        if (vGuardMgr == null && activity is MainActivity) {
+            setupVGuard(activity)
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        vGuardMgr?.onResume(hook)
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        vGuardMgr?.onPause(hook)
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (activity is MainActivity) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastRcvr)
+            vGuardMgr?.destroy()
+            vGuardMgr = null
+        }
+    }
+
+    override fun onActivityStarted(activity: Activity) {}
+
+    override fun onActivityStopped(activity: Activity) {}
+
+    override fun onActivitySaveInstanceState(activity: Activity, savedInstanceState: Bundle) {}
+}
+
---
+++ app/src/main/java/id/co/sistema/vkey/HomeActivity.kt
@@ -0,0 +1,262 @@
+package id.co.sistema.vkey
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.vkey.android.vguard.VGException
+import com.vkey.securefileio.FileOutputStream
+import com.vkey.securefileio.SecureFile
+import com.vkey.securefileio.SecureFileIO
+import vkey.android.vos.Vos
+import vkey.android.vos.VosWrapper
+import java.io.File
+import java.io.FileInputStream
+
+class HomeActivity : AppCompatActivity() , VosWrapper.Callback{
+
+    private lateinit var mVos: Vos
+    private lateinit var mStartVosThread: Thread
+    private lateinit var tvMessage: TextView
+    private var encryptedFileLocation = ""
+
+    companion object {
+        private const val TAG = "HelloActivity"
+        private const val TAG_SFIO = "SecureFileIO"
+        private const val STR_INPUT = "Quick brown fox jumps over the lazy dog. 1234567890 some_one@somewhere.com"
+        private const val PASSWORD = "P@ssw0rd"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        tvMessage = findViewById(R.id.tv_message)
+
+        mVos = Vos(this)
+        mVos.registerVosWrapperCallback(this)
+        startVos(this)
+
+
+        encryptDecryptByteFile()// berhasil
+        encryptDecryptBlockData()//berhasil
+        encryptDecryptStringFile()//berhasil
+        encryptExistingFile()//berhasil
+        writeReadEncryptedFile()
+        encryptExistingFileNonText()
+
+    }
+
+    private fun startVos(ctx: Context) {
+        mStartVosThread = Thread {
+            try {
+                // Get the kernel data in byte from `firmware` asset file
+                val inputStream = ctx.assets.open("firmware")
+                val kernelData = inputStream.readBytes()
+                inputStream.read(kernelData)
+                inputStream.close()
+
+                // Start V-OS
+                val vosReturnCode = mVos.start(kernelData, null, null, null, null)
+
+                if (vosReturnCode > 0) {
+                    // Successfully started V-OS
+                    // Instantiate a `VosWrapper` instance for calling V-OS Processor APIs
+                    val vosWrapper = VosWrapper.getInstance(ctx)
+                    val version = vosWrapper.processorVersion
+                    val troubleShootingID = String(vosWrapper.troubleshootingId)
+                    Log.d(
+                        TAG,
+                        "ProcessorVers: $version || TroubleShootingID: $troubleShootingID"
+                    )
+                } else {
+                    // Failed to start V-OS
+                    Log.e(TAG, "Failed to start V-OS")
+                }
+            } catch (e: VGException) {
+                Log.e(TAG, e.message.toString())
+                e.printStackTrace()
+            }
+        }
+
+        mStartVosThread.start()
+    }
+
+    private fun stopVos() {
+        mVos.stop()
+    }
+
+    /**
+     * SFIO OPERATIONS - Encrypt/Decrypt Block of Data
+     * The APIs for encrypting and decrypting block of data are part of the SecureFileIO class.
+     * */
+    private fun encryptDecryptBlockData() {
+        try {
+            // The block of data in byte
+            val input: ByteArray = STR_INPUT.toByteArray()
+
+            // Encrypt the block of data
+            val chiper: ByteArray = SecureFileIO.encryptData(input)
+
+            // Decrypt the block encrypted block of data
+            val decrypted = SecureFileIO.decryptData(chiper)
+            val decryptedInput = String(decrypted)
+            Log.d("blockdata", decryptedInput)
+        } catch (e: Exception) {
+            Log.e("blockdata", e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * SFIO OPERATIONS - Encrypting/Decrypting a String to/from a File
+     * The APIs for encrypting and decrypting string to/from files are part of the SecureFileIO class.
+     * */
+    private fun encryptDecryptStringFile() {
+        try {
+            // the path to the encrypted file
+            val encryptedFilePath = "${this.filesDir.absolutePath}/encryptedFile.txt"
+
+            // Write the string to the encrypted file. If you do not wish to set a
+            // password, use an empty string like "" instead. Setting the last
+            // parameter to `true` will write the file atomically.
+            SecureFileIO.encryptString(STR_INPUT, encryptedFilePath, PASSWORD, false)
+
+            // Decrypt the encrypted file in the string format
+            val decryptedString = SecureFileIO.decryptString(encryptedFilePath, PASSWORD)
+            tvMessage.text = decryptedString
+            Log.d("sfile", decryptedString)
+        } catch (e: Exception) {
+            Log.e("sfile", e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * SFIO OPERATIONS - Encrypting/Decrypting a Block of Data to/from a File
+     * The APIs for encrypting and decrypting block data to/from files are part of the SecureFileIO class.
+     * */
+    private fun encryptDecryptByteFile() {
+        try {
+            val input = STR_INPUT.toByteArray()
+
+            // The path to the encrypted file
+            val encryptedFilePath = "${this.filesDir.absolutePath}/encryptedFile.txt"
+
+            // Write the block data to the encrypted file. If you do not wish to set a
+            // password, use an empty string like "" instead. Setting the last
+            // parameter to `true` will write the file atomically.
+            SecureFileIO.encryptData(input, encryptedFilePath, PASSWORD, false)
+
+            // Decrypt the encrypted file in the byte format
+            val decrypted = SecureFileIO.decryptFile(encryptedFilePath, PASSWORD)
+            val decryptedResult = String(decrypted)
+
+            Log.d("bfile", decryptedResult)
+        } catch (e: Exception) {
+            Log.e("bfile", e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+    private fun prepareFiles() {
+        val dirLocation = File(applicationContext?.filesDir?.absolutePath + "/sistema")
+        if(!dirLocation.exists()) dirLocation.mkdir()
+
+        encryptedFileLocation = "$dirLocation/fileEncryption.txt"
+        val file = File(encryptedFileLocation)
+        if(!file.exists()) {
+            Log.d("prepare","Creating new file")
+            file.createNewFile()
+        } else {
+            Log.d("prepare","Creating new file")
+        }
+    }
+
+
+    /**
+     * SFIO OPERATIONS - Encrypting an Existing File
+     * The API for encrypting an existing file is part of the SecureFileIO class.
+     * */
+    private fun encryptExistingFile() {
+        try {
+            val encryptedFilePath = "${this.filesDir.absolutePath}/encryptedFile.txt"
+                java.io.FileOutputStream(encryptedFilePath).use {
+                it.write(STR_INPUT.toByteArray())
+                it.close()
+            }
+
+
+            SecureFileIO.encryptFile(encryptedFilePath , PASSWORD)
+            val decrypt = SecureFileIO.decryptFile(encryptedFilePath , PASSWORD)
+
+
+            Log.d("existfile", "test $decrypt")
+
+        } catch (e: Exception) {
+            Log.e("existfile", e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+    private fun encryptExistingFileNonText() {
+        try {
+            val encryptedFilePath = "${this.filesDir.absolutePath}/wojak.jpg"
+            java.io.FileOutputStream(encryptedFilePath).use {
+                it.write(R.drawable.wojak)
+                it.close()
+            }
+
+            SecureFileIO.encryptFile(encryptedFilePath , PASSWORD)
+            val decrypt  =SecureFileIO.decryptFile(encryptedFilePath , PASSWORD)
+
+            Log.d("existfilee", "test $decrypt")
+
+        } catch (e: Exception) {
+            Log.e("existfilee", e.message.toString())
+            e.printStackTrace()
+        }
+    }
+
+
+    private fun createEncryptedFile(filePath: String) {
+        val osStream = FileOutputStream(filePath, PASSWORD)
+        osStream.close()
+
+        // Supported flags
+        val flags = SecureFile.O_CREAT or SecureFile.O_TRUNC or SecureFile.O_RDWR;
+        // Permissions must be given only when the flag is set for O_CREAT
+        val permission =
+            SecureFile.S_IRUSR or SecureFile.S_IWUSR or SecureFile.S_IRGRP or SecureFile.S_IROTH;
+        val secureFile = SecureFile(filePath, PASSWORD, flags, permission)
+        secureFile.close()
+    }
+
+
+    /**
+     * SFIO OPERATIONS: Writing/Reading to/from an Encrypted File
+     * The API for writing/reading to/from an encrypted file is
+     * part of the FileInputStream and FileOutputStream class
+     * */
+    private fun writeReadEncryptedFile() {
+        // If you do not wish to set a password, use an empty string
+        // like "" instead. This will create the file if it does not
+        // exist, and if it does, it will overwrite.
+        val filePath = "${this.filesDir.absolutePath}/encryptedFile.txt"
+        val ostream = FileOutputStream(filePath, PASSWORD)
+        ostream.write("Hello ".toByteArray())
+        ostream.write("World".toByteArray())
+        ostream.close()
+
+        // If you do not wish to set a password, use an empty string
+        // like "" instead.
+
+    }
+
+    override fun onNotified(p0: Int, p1: Int): Boolean {
+        Log.d(TAG, "onNotified: ")
+        return true
+    }
+
+}
